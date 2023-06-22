3baa95707dfcfca7b4a33f8f3eedf701c349ae1c
Mohd Zaid Khan <111868309+zeddkhan03@users.noreply.github.com>
2023-06-22 11:45:24 +0530

    Update index.php

--- reg&login/index.php
+++ reg&login/index.php
@@ -50,7 +50,8 @@ session_start();
           <button type="reset" onclick="popup('login-popup')">X</button>
         </h2>
         <input type="text" placeholder="E-mail or Username" name="email_username">
-        <input type="password" placeholder="Password" name="password">
+        <input type="password" placeholder="Password" name="password" id="login-password">
+        <span class="password-toggle" onclick="togglePassword('login-password')">Show</span>
         <button type="submit" class="login-btn" name="login">LOGIN</button>
       </form>
     </div>
@@ -63,10 +64,12 @@ session_start();
           <span>USER REGISTER</span>
           <button type="reset" onclick="popup('register-popup')">X</button>
         </h2>
-        <input type="text" placeholder="Full Name" name="fullname">
-        <input type="text" placeholder="Username" name="username">
-        <input type="email" placeholder="E-mail" name="email">
-        <input type="password" placeholder="Password" name="password">
+        <input type="text" placeholder="Full Name" name="fullname" required>
+        <input type="text" placeholder="Username" name="username" required>
+        <input type="email" placeholder="E-mail" name="email" required>
+        <input type="password" placeholder="Password" name="password" id="register-password" required>
+        <span class="password-toggle" onclick="togglePassword('register-password')">Show</span>
+        <input type="text" placeholder="Referral Code" name="referralcode" id="refercode">
         <button type="submit" class="register-btn" name="register">REGISTER</button>
       </form>
     </div>
@@ -76,6 +79,15 @@ session_start();
     if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true)
     {
       echo "<h1 style='text-align: center; margin-top:200px'>Welcome - $_SESSION[username]</h1>";
+
+      $query="SELECT * FROM `registered_users` WHERE `username`='$_SESSION[username]'";
+      $result=mysqli_query($con, $query);
+      $result_fetch=mysqli_fetch_assoc($result);
+
+      echo"<h3 class='box'>Your referral code: $result_fetch[referral_code]</h3>";
+      echo"<h3 class='box'>Your referral points: $result_fetch[referral_point]</h3>";
+      echo"<h3 class='box'>Your referral link: <a href='http://localhost/projects/reg&login/index.php?refer=$result_fetch[referral_code]'> http://localhost/projects/reg&login/index.php?refer=$result_fetch[referral_code]</a></h3>";
+
     }
   ?>
 
@@ -93,7 +105,42 @@ session_start();
         get_popup.style.display="flex";
       }
     }
+
+    function togglePassword(inputId) {
+      const passwordInput = document.getElementById(inputId);
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+      } else {
+        passwordInput.type = "password";
+      }
+    }
   </script>
+
+  <?php
+    if(isset($_GET['refer']) && $_GET['refer']!='')
+    {
+      if(!(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true))
+      {
+        $query = "SELECT * FROM `registered_users` WHERE `referral_code`='{$_GET['refer']}'";
+        $result = mysqli_query($con, $query);
+        if(mysqli_num_rows($result) == 1)
+        {
+          echo "
+          <script>
+            document.getElementById('refercode').value = '{$_GET['refer']}';
+            popup('register-popup');
+          </script>";
+        }
+        else
+        {
+          echo "
+          <script>
+            alert('Invalid referral code');
+          </script>";
+        }
+      }
+    }
+  ?>
 
 </body>
 </html>
